9dc8ab2e245eef3702a4031533ac9cba581b39d0
Ulises Bocchio <ubocchio@ULINEWPRO.local>
2017-08-22 02:39:27 -0700

    slice container

--- src/components/TodoList/TodoListContainer.js
+++ src/components/TodoList/TodoListContainer.js
@@ -1,32 +1,31 @@
-import { connectSlice } from 'react-redux-boilerout';
+import { sliceContainer } from 'react-redux-boilerout';
 import TodosActions from '../../actions/TodosActions';
 import TodoList from './TodoList';
 
-const getVisibleTodos = (todos, filter) => {
-    switch (filter) {
-        case 'SHOW_ALL':
-            return todos;
-        case 'SHOW_COMPLETED':
-            return todos.filter(t => t.completed);
-        case 'SHOW_ACTIVE':
-            return todos.filter(t => !t.completed);
-        default:
-            throw new Error('Unknown filter: ' + filter)
+
+@sliceContainer({ slice: 'todos', actions: TodosActions, component: TodoList })
+export default class VisibleTodoList {
+
+    getVisibleTodos = (todos, filter) => {
+        switch (filter) {
+            case 'SHOW_ALL':
+                return todos;
+            case 'SHOW_COMPLETED':
+                return todos.filter(t => t.completed);
+            case 'SHOW_ACTIVE':
+                return todos.filter(t => !t.completed);
+            default:
+                throw new Error('Unknown filter: ' + filter)
+        }
+    };
+
+    mapSliceStateToProps(state) {
+        return {
+            todos: this.getVisibleTodos(state.items, state.filter)
+        }
     }
-};
 
-const mapSliceStateToProps = (state) => ({
-    todos: getVisibleTodos(state.items, state.filter)
-});
+    inject() {
 
-const inject = {};
-
-const VisibleTodoList = connectSlice({
-        slice: 'todos',
-        actions: TodosActions,
-        inject
-    },
-    mapSliceStateToProps
-)(TodoList);
-
-export default VisibleTodoList
+    }
+}
