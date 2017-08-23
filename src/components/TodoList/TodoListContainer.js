97c4af85283c9b6a515ef41a8eec3abdf0fa90dd
Ulises Bocchio <ubocchio@ULINEWPRO.local>
2017-08-23 03:57:54 -0700

    hoister on class

--- src/components/TodoList/TodoListContainer.js
+++ src/components/TodoList/TodoListContainer.js
@@ -2,9 +2,12 @@ import { sliceContainer } from 'react-redux-boilerout';
 import TodosActions from '../../actions/TodosActions';
 import TodoList from './TodoList';
 
-
 @sliceContainer({ slice: 'todos', actions: TodosActions, component: TodoList })
 export default class VisibleTodoList {
+
+    componentWillMount() {
+        console.log('Todos Container mounted');
+    }
 
     static getVisibleTodos = (todos, filter) => {
         switch (filter) {
