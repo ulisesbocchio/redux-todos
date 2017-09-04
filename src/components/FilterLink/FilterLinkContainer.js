644a3c5dc4d5dc075b0442844f6b1cbc1ebd0640
Ulises Bocchio <ubocchio@ULINEWPRO.local>
2017-09-04 15:41:05 -0700

    decorators

--- src/components/FilterLink/FilterLinkContainer.js
+++ src/components/FilterLink/FilterLinkContainer.js
@@ -1,22 +1,16 @@
-import { connectSlice } from 'react-redux-boilerout';
+import { sliceContainer } from 'react-redux-boilerout';
 import TodosActions from '../../actions/TodosActions';
 import FilterLink from './FilterLink';
 
-const mapStoreStateToProps = (state, ownProps) => ({
-  active: ownProps.filter === state.filter
-});
+@sliceContainer({ slice: 'todos', actions: TodosActions, component: FilterLink })
+export default class FilterLinkContainer {
+    static mapStoreStateToProps = (state, ownProps) => ({
+        active: ownProps.filter === state.filter
+    });
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  onClick: () => {
-      TodosActions.setVisibilityFilter(ownProps.filter);
-  }
-});
-
-const FilterLinkContainer = connectSlice({
-    slice: 'todos'
-  },
-    mapStoreStateToProps,
-    mapDispatchToProps
-)(FilterLink);
-
-export default FilterLinkContainer
+    static mapDispatchToProps = (dispatch, ownProps) => ({
+        onClick: () => {
+            TodosActions.setVisibilityFilter(ownProps.filter);
+        }
+    });
+}
